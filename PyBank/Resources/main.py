7c611135acce76f77e1a52327fdc9698d0e63540
Michelle Fegatelli <chellefig17@gmail.com>
2020-11-23 17:58:34 -0500

    Find total months

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -3,6 +3,7 @@
 import os
 # Module for reading CSV files
 import csv
+import datetime
 csvpath = os.path.join('budget_data.csv')
 # # Method 1: Plain Reading of CSV files
 # with open(csvpath, ‘r’) as file_handler:
@@ -20,7 +21,18 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     for row in csvreader:
         print(row)
-# get the start date in the data set
+# set the dates to find the total number of months in the dataset
 import datetime
-x = datetime.datetime.now()
-print (x)
+end_date = datetime.datetime(2017,2,28)
+start_date = datetime.datetime(2009,12, 31)
+num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
+print (f"Total Months: {num_months}")
+# find the net total of profit and loss
+#Import Fin file
+with open('budget_data.csv') as fin:
+    headerline = fin.next()
+    total = 0
+    for row in csv.reader(fin):
+        total += int(row[1])
+print (f"Total Months: {total}")    
+
