b82894fddf1f2ccee7452674907a446367bbbcaa
Michelle Fegatelli <chellefig17@gmail.com>
2020-11-24 15:30:37 -0500

    Update main.py

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -5,18 +5,19 @@ import os
 import csv
 import datetime
 csvpath = os.path.join('budget_data.csv')
-total_pl = 0
+total_pl =0
 # Method 2: Improved Reading using CSV module
 with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    # print(csvreader)  
+    print(csvreader)  
 # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     # Read each row of data after the header
     for row in csvreader:
-        month = row[0]
+        print(row)
+        montht = row[0]
         profit_loss = int(row[1])
         total_pl += profit_loss
 # set the dates to find the total number of months in the dataset
@@ -25,7 +26,5 @@ end_date = datetime.datetime(2017,2,28)
 start_date = datetime.datetime(2009,12, 31)
 num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
 print (f"Total Months: {num_months}")
-print(total_pl)
-# Find the Net Total of Profit/Loss
-# Read each row of data after the header
- 
+print (f"Total: ${total_pl}")
+
