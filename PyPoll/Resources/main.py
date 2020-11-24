51e751362f12ce0d8578ab92310e011cb08d59ac
Michelle Fegatelli <chellefig17@gmail.com>
2020-11-24 16:32:17 -0500

    Create main.py

---
+++ PyPoll/Resources/main.py
@@ -0,0 +1,17 @@
+# import the os module
+# This will allow us to create file paths across operating systems
+import os
+# Module for reading CSV files
+import csv
+import datetime
+csvpath = os.path.join('election_data_csv')
+total_revenue =0
+# Method 2: Improved Reading using CSV module
+with open(csvpath) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)  
+# Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header
